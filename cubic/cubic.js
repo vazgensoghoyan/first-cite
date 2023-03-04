d329c345fe0be5df97fc87e3a91d9b8883e58950
Vazgen Soghoyan <sogvazulchik@gmail.com>
2023-03-05 02:25:54 +0500

    cubic equations

--- cubic/cubic.js
+++ cubic/cubic.js
@@ -1,3 +1,78 @@
+class Complex {
+    constructor (a, b) {
+        this.re = a;
+        this.im = b;
+    }
+
+    get Modulus() {
+        return Math.sqrt(this.re * this.re + this.im * this.im);
+    }
+
+    get Arg () {
+        const atan = Math.atan(this.im / this.re);
+        if (this.re >= 0) return atan;
+        if (this.im <= 0) return atan - Math.PI;
+        return atan + Math.PI;
+    }
+
+    static sum() {
+        let re = 0;
+        let im = 0;
+
+        for (var i = 0; i < arguments.length; i++) {
+            re += arguments[i].re;
+            im += arguments[i].im;
+        }
+
+        return new Complex(re, im);
+    }
+
+    static mult(n1, n2) {
+        return new Complex(n1.re * n2.re - n1.im * n2.im,
+                        n1.im * n2.re + n1.re * n2.im);
+    }
+
+    static div(n1, n2) {
+        const a = n2.Modulus * n2.Modulus;
+
+        return new Complex((n1.re * n2.re + n1.im * n2.im) / a,
+                        (n1.im * n2.re - n1.re * n2.im) / a);
+    }
+
+    min() {
+        return new Complex(-this.re, -this.im);
+    }
+
+    static takeRoot(number, n) {
+        let answers = [];
+        let module = Math.pow(number.Modulus, 1 / n);
+        let initialArg = number.Arg / n;
+        if (number.im == 0 & number.re == 0) {
+            initialArg = 0;
+        }
+        for (var i = 0; i < n; i++) {
+            answers[i] = new Complex(
+                module * Math.cos(initialArg + 2 * Math.PI * i / n),
+                module * Math.sin(initialArg + 2 * Math.PI * i / n)
+            );
+        }
+        return answers;
+    }
+
+    pow(n) {
+        return new Complex(
+            Math.pow(this.Modulus, n) * Math.cos(n * this.Arg),
+            Math.pow(this.Modulus, n) * Math.sin(n * this.Arg)
+        );
+    }
+
+    toString() {
+        const accuracy = 5;
+        return parseFloat(this.re.toFixed(accuracy)) + " + i * " + parseFloat(this.im.toFixed(accuracy));
+    }
+}
+
+
 let input1 = document.getElementById('a');
 let input2 = document.getElementById('b');
 let input3 = document.getElementById('c');
@@ -44,10 +119,10 @@ function solveComplexEquation3(a, b, c, d) {
 
     let answers = [];
     for (var i = 0; i < f.length; i++) {
-        answers[i] = Complex.add(f[i], Complex.div(new Complex(p, 0), Complex.mult(new Complex(3, 0), f[i])), new Complex(shift, 0));
+        answers[i] = Complex.sum(f[i], Complex.div(new Complex(p, 0), Complex.mult(new Complex(3, 0), f[i])).min(), new Complex(shift, 0));
     }
     
-    return ans;
+    return answers;
 }
 
 function printResult() {
