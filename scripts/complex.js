146f6cdef45b71677d6645f4ced8d761ab95d43f
Vazgen Soghoyan <sogvazulchik@gmail.com>
2023-03-04 23:14:11 +0500

    sum and Arg in complex

--- scripts/complex.js
+++ scripts/complex.js
@@ -1,23 +1,29 @@
 class Complex {
     constructor (a, b) {
-        this.im = parseFloat(a);
-        this.re = parseFloat(b);
+        this.re = parseFloat(a);
+        this.im = parseFloat(b);
     }
 
     get Modulus() {
         return Math.sqrt(this.re * this.re + this.im * this.im);
     }
 
+    get Arg () {
+        const atan = Math.atan(this.im / this.re);
+        if (this.re >= 0) return atan;
+        if (this.im <= 0) return atan - Math.PI;
+        return atan + Math.PI;
+    }
+
     static sum() {
         let a = 0;
+        let b = 0;
 
         for (var i = 0; i < arguments.length; i++) {
-            a += arguments[i];
+            a += arguments[i].re;
+            b += arguments[i].im;
         }
 
-        return a;
+        return new Complex(a, b);
     }
 }
-
-const n1 = new Complex(3, 4);
-console.log(n1.Modulus)
